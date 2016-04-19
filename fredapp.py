92899599dc84c249b1ab75027ff3f6876757866b
PaulPruliere <prulierepaul@gmail.com>
2016-04-19 12:28:50 +0200

    superficial changes

--- fredapp.py
+++ fredapp.py
@@ -1,12 +1,9 @@
-import os
 from flask import Flask, render_template
 from fetching import fetcher
 
 app = Flask(__name__)
 app.debug = True
 
-
-@app.route('/', defaults={'path': ''})
 @app.route('/probabilities/<path:path>')
 def probabilities(path):
 	spreadsheet = "dataset-code-challenge - personalities_data.csv"
