2a2110cf4b0e9ace9c0b4c3b2743be394fcf7b4a
PaulPruliere <prulierepaul@gmail.com>
2016-04-27 11:26:48 +0200

    docstring

--- fetching.py
+++ fetching.py
@@ -4,41 +4,41 @@ from collections import Counter
 import json
 import requests
 
-# import data spreadsheet as dataframe and reshape it
 def import_data(spreadsheet):
+    """Import data spreadsheet as dataframe and reshape it."""
     df = pd.read_csv(spreadsheet,header=1,index_col="Unnamed: 1")
     del df["Unnamed: 0"]
     df = df.T
     return df
 
-# extract request attributes
 def get_request_attributes(path):
+    """Extract request attributes."""
     attributes = path.split('/')
     return attributes
 
-# count how offen attributes match
 def count_matches(path,df):
+    """Count how offen attributes match."""
     attributes = get_request_attributes(path)
-    attributes = [ a for a in attributes if a in df.columns.values] # keep valid attributes 
+    attributes = [a for a in attributes if a in df.columns.values] #keep valid attributes only
     matches = []
     for attr in attributes:
         matches.extend(df.loc[df["{}".format(attr)]=="x"].index)
     matches = dict(Counter(matches))
     return matches
 
-# normalize counter values to add up up to 1
 def normalize(counter):
+    """Normalize counter values to add up up to 1."""
     norm = sum(counter.values())
     counter.update((x,float(y)/norm) for x,y in counter.items())
     return counter
 
-# change counter to json array
 def jsonify_counter(counter):
+    """Change counter to json array."""
     jarray = json.dumps([{"personality":i,"probability":j} for i,j in counter.items()])
     return jarray
 
-# wrap up functions above return json array
 def fetcher(path,spreadsheet):
+    """Wrap up functions above return json array."""
     df = import_data(spreadsheet)
     probabilities = normalize(count_matches(path,df))
     return jsonify_counter(probabilities)
